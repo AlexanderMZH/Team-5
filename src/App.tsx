a0ab9a5fba5fee8a6e3279666bb90da6b43b6905
AlexMZH98 <alex.mzhava@gmail.com>
2024-03-12 23:11:46 +0400

    design update

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
 import "./App.css";
 
-import { useState } from "react";
-import Spinner from "./Components/Spinner";
 import Layout from "./Layout";
 
 function App() {
-  const [spinnerToggle, setSpinnerToggle] = useState(false);
-
   return (
     <div className="App">
       <Layout />
--- src/Pages/SingleItem/SingleItem.tsx
+++ src/Pages/SingleItem/SingleItem.tsx
@@ -1,17 +1,103 @@
+import { useState } from "react";
+
 import "./style.scss";
 
 const SingleItem = () => {
+  const [allActive, setAllActive] = useState("active");
+  const [dressesActive, setDressesActive] = useState("");
+  const [topsActive, setTopsActive] = useState("");
+  const [outerWearActive, setOuterWearActive] = useState("");
+  const [jacketActive, setJacketActive] = useState("");
+
+  const productNavActiveToggle = (param: String) => {
+    setAllActive("");
+    setDressesActive("");
+    setTopsActive("");
+    setOuterWearActive("");
+    setJacketActive("");
+    if (param === "all") {
+      setAllActive((preState) => (preState === "" ? "active" : ""));
+    } else if (param === "dresses") {
+      setDressesActive((preState) => (preState === "" ? "active" : ""));
+    } else if (param === "tops") {
+      setTopsActive((preState) => (preState === "" ? "active" : ""));
+    } else if (param === "outerwear") {
+      setOuterWearActive((preState) => (preState === "" ? "active" : ""));
+    } else if (param === "jacket") {
+      setJacketActive((preState) => (preState === "" ? "active" : ""));
+    }
+  };
+
   return (
     <>
-      <section className="single-item-section">
-        <div className="single-item-container">
-          <ul className="single-item-container-ul">
-            <li className="single-item-container-li">
-              <div className="single-item">
-                <div className="single-item-image">
+      <section className="popular-product-section">
+        <div className="popular-product-nav">
+          <div>
+            <h1>Most Popular Products</h1>
+          </div>
+          <div className="product-nav">
+            <ul>
+              <li
+                className={allActive}
+                onClick={() => {
+                  productNavActiveToggle("all");
+                }}
+              >
+                <div>
+                  <span>all</span>
+                </div>
+              </li>
+              <li
+                className={dressesActive}
+                onClick={() => {
+                  productNavActiveToggle("dresses");
+                }}
+              >
+                <div>
+                  <span>Dresses</span>
+                </div>
+              </li>
+              <li
+                className={topsActive}
+                onClick={() => {
+                  productNavActiveToggle("tops");
+                }}
+              >
+                <div>
+                  <span>tops</span>
+                </div>
+              </li>
+              <li
+                className={outerWearActive}
+                onClick={() => {
+                  productNavActiveToggle("outerwear");
+                }}
+              >
+                <div>
+                  <span>outerwear</span>
+                </div>
+              </li>
+              <li
+                className={jacketActive}
+                onClick={() => {
+                  productNavActiveToggle("jacket");
+                }}
+              >
+                <div>
+                  <span>jacket</span>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="popular-product-container">
+          <ul className="popular-product-container-ul">
+            <li className="popular-product-container-li">
+              <div className="popular-product">
+                <div className="popular-product-image">
                   <img src="./dress.png" alt="" />
                 </div>
-                <div className="single-item-features">
+                <div className="popular-product-features">
                   <div className="features-text">
                     <span>get 20% off</span>
                   </div>
@@ -20,21 +106,21 @@ const SingleItem = () => {
                     <img src="./cart.png" alt="" />
                   </div>
                 </div>
-                <div className="view active">
+                <div className="view">
                   <span>quick view</span>
                 </div>
-                <div className="single-item-name">
+                <div className="popular-product-name">
                   <h3>cazy knit cardigan sweater</h3>
                   <h3>80$</h3>
                 </div>
               </div>
             </li>
-            <li className="single-item-container-li">
-              <div className="single-item">
-                <div className="single-item-image">
+            <li className="popular-product-container-li">
+              <div className="popular-product">
+                <div className="popular-product-image">
                   <img src="./dress.png" alt="" />
                 </div>
-                <div className="single-item-features">
+                <div className="popular-product-features">
                   <div className="features-text">
                     <span>get 20% off</span>
                   </div>
@@ -43,21 +129,21 @@ const SingleItem = () => {
                     <img src="./cart.png" alt="" />
                   </div>
                 </div>
-                <div className="view active">
+                <div className="view ">
                   <span>quick view</span>
                 </div>
-                <div className="single-item-name">
+                <div className="popular-product-name">
                   <h3>cazy knit cardigan sweater</h3>
                   <h3>80$</h3>
                 </div>
               </div>
             </li>
-            <li className="single-item-container-li">
-              <div className="single-item">
-                <div className="single-item-image">
+            <li className="popular-product-container-li">
+              <div className="popular-product">
+                <div className="popular-product-image">
                   <img src="./dress.png" alt="" />
                 </div>
-                <div className="single-item-features">
+                <div className="popular-product-features">
                   <div className="features-text">
                     <span>get 20% off</span>
                   </div>
@@ -66,21 +152,21 @@ const SingleItem = () => {
                     <img src="./cart.png" alt="" />
                   </div>
                 </div>
-                <div className="view active">
+                <div className="view ">
                   <span>quick view</span>
                 </div>
-                <div className="single-item-name">
+                <div className="popular-product-name">
                   <h3>cazy knit cardigan sweater</h3>
                   <h3>80$</h3>
                 </div>
               </div>
             </li>
-            <li className="single-item-container-li">
-              <div className="single-item">
-                <div className="single-item-image">
+            <li className="popular-product-container-li">
+              <div className="popular-product">
+                <div className="popular-product-image">
                   <img src="./dress.png" alt="" />
                 </div>
-                <div className="single-item-features">
+                <div className="popular-product-features">
                   <div className="features-text">
                     <span>get 20% off</span>
                   </div>
@@ -89,10 +175,10 @@ const SingleItem = () => {
                     <img src="./cart.png" alt="" />
                   </div>
                 </div>
-                <div className="view active">
+                <div className="view">
                   <span>quick view</span>
                 </div>
-                <div className="single-item-name">
+                <div className="popular-product-name">
                   <h3>cazy knit cardigan sweater</h3>
                   <h3>80$</h3>
                 </div>
